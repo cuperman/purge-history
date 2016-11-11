d979f9ded8c5c933d38a65294aa2c1bd5edcb678
Jeff Cooper <jeff@cuperman.net>
2016-11-10 18:34:55 -0800

    Create Chrome extension

---
+++ src/js/purge.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var submitPurge = document.getElementById('submit-purge');
+
+  submitPurge.addEventListener("click", function() {
+    debugger;
+  });
+});
