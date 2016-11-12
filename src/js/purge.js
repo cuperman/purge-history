71c7f320e1413fbd921112892ce862f305216884
Jeff Cooper <jeff@cuperman.net>
2016-11-11 18:27:51 -0800

    Delete all URLs since a specific date

--- src/js/purge.js
+++ src/js/purge.js
@@ -1,29 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
   var submitPurge = document.getElementById('submit-purge');
+  var inputDomain = document.getElementById('input-domain');
+  var inputSince  = document.getElementById('input-since');
 
   submitPurge.addEventListener('click', function(ev) {
     ev.preventDefault();
 
-    var inputDomain = document.getElementById('input-domain');
     var domain = inputDomain.value;
+    var sinceString = inputSince.value;
 
-    if (domain.match(/([\w-]+\.)*[\w-]+\.[\w]+/)) {
-      chrome.history.search({ text: domain, maxResults: 0 }, function(results) {
-        var domainMatcher = new RegExp('^https?://([\\w-]+\\.)*' + domain);
-        var counter = 0;
+    var sinceDate = Date.parse(sinceString);
+    var domainMatcher = new RegExp('([\\w-]+\\.)*[\\w-]+\\.[\\w]+');
 
-        results.forEach(function(result, index, allResults) {
-          var url = result.url;
-          if (url.match(domainMatcher)) {
-            counter++;
-            chrome.history.deleteUrl({ url: url });
-          }
-        });
-
-        document.write('Success: deleted ' + counter + ' URLs');
-      });
-    } else {
-      document.write('Error: ' + domain + ' is not a valid domain');
+    if (!domain.match(domainMatcher)) {
+      document.write('Error: \'' + domain + '\' is not a valid domain');
+      return false;
     }
+
+    if (isNaN(sinceDate)) {
+      document.write('Error: \'' + sinceString + '\' is not a valid date');
+      return false;
+    }
+
+    chrome.history.search({ text: domain, startTime: sinceDate, maxResults: 0 }, function(results) {
+      var urlMatcher = new RegExp('^https?://([\\w-]+\\.)*' + domain);
+      var counter = 0;
+
+      results.forEach(function(result, index, allResults) {
+        var url = result.url;
+        if (url.match(urlMatcher)) {
+          counter++;
+          chrome.history.deleteUrl({ url: url });
+        }
+      });
+
+      document.write('Success: deleted ' + counter + ' URLs');
+    });
   });
 });
